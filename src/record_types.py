2b2ba2a4c7c3426d909f90e1b7d4ae52fdf15cf1
rdkc4 <rdakic@protonmail.com>
2026-06-12 08:43:19 +0200

    Make dashboard track multiple workloads

--- src/record_types.py
+++ src/record_types.py
@@ -1,6 +1,6 @@
 from typing import TypeAlias
 
-Record:           TypeAlias = dict[str, float]
-RecordList:       TypeAlias = list[Record]
-RecordGroup:      TypeAlias = dict[str, RecordList]
-FlatRecords:      TypeAlias = dict[str, list[float]]
+Record:      TypeAlias = dict[str, float]
+RecordList:  TypeAlias = list[Record]
+RecordGroup: TypeAlias = dict[str, RecordList]
+FlatRecords: TypeAlias = dict[str, list[float]]
